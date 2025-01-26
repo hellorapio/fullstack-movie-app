897211171ef3e046a15adde3e34006238a0a3b9e
Anubis <anubis356@outlook.com>
2025-01-26 18:30:45 +0200

    fix: removing a trailing slash

--- backend/src/guards/logged.guard.ts
+++ backend/src/guards/logged.guard.ts
@@ -23,8 +23,7 @@ export default class LoggedGuard implements CanActivate {
     // JWT auth should be added in real world case
     // this is just a test
 
-    if (!req.headers.authorization) return false;\
-    
+    if (!req.headers.authorization) return false;
     if (req.headers.authorization.startsWith('authenticated')) return true;
     return false;
   }
