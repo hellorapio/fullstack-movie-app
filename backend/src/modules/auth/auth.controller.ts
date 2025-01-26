080f42d1c6995b1fd2bdaa5c11fcb2d1fd20e394
Anubis <anubis356@outlook.com>
2025-01-26 17:22:12 +0200

    feat: finishing dummy auth routes

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,15 +1,24 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import SignupDTO from './dto/signup.dto';
+import LoginDTO from './dto/login.dto';
+import Public from 'src/decorators/Public.decorator';
 
+@Public()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-up')
-  async signup(@Body() body: SignupDTO) {}
+  async signup(@Body() body: SignupDTO) {
+    const user = await this.authService.signup(body);
+    return { status: 'success', message: 'User created', data: user };
+  }
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
-  async login(@body) {}
+  async login(@Body() body: LoginDTO) {
+    await this.authService.login(body);
+    return { status: 'success', message: 'User logged in', data: {} };
+  }
 }
--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import SignupDTO from './dto/signup.dto';
+import LoginDTO from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async signup(data) {
-    return await this.prisma.user.create({ data });
+  async signup(data: SignupDTO) {
+    const user = await this.prisma.user.create({ data });
+    delete user.password;
+    return user;
+  }
+
+  async login(data: LoginDTO) {
+    const user = await this.prisma.user.findFirst({
+      where: { email: data.email },
+    });
+
+    if (user.password !== data.password)
+      throw new UnauthorizedException('the password is incorrect');
+
+    return user;
   }
 }
