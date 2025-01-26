02c2f3435f96fe95e1ca64a262c69e95abaeb67d
Anubis <anubis356@outlook.com>
2025-01-26 18:07:14 +0200

    fix: adjusting database favorites table, the gaurd and user decorator

--- backend/src/decorators/User.decorator.ts
+++ backend/src/decorators/User.decorator.ts
@@ -1,8 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
 
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user;
+    const request = ctx.switchToHttp().getRequest() as Request;
+    return request.headers.authorization.split(' ')[1];
   },
 );
--- backend/src/guards/logged.guard.ts
+++ backend/src/guards/logged.guard.ts
@@ -3,14 +3,16 @@ import { Request } from 'express';
 import { Observable } from 'rxjs';
 
 export default class LoggedGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const req = context.switchToHttp().getRequest() as Request;
-    
+
     // Dummy authorization for faster development
     // JWT auth should be added in real world case
     // this is just a test
-    
-    if (req.headers.authorization !== "authenticated") return false;
-    return true;
+
+    if (req.headers.authorization.startsWith('authenticated')) return true;
+    return false;
   }
 }
